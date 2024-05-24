ca629f7366c048ed2d2ccbbcb705c5fda2df9c3b
Rob Hough <robhough180@gmail.com>
2024-05-24 12:14:30 +0200

    Started PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
